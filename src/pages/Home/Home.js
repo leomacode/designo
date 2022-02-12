6954d9c343af7df0003ca84de92b8250ca05bc35
leomacode <forza.leoma@gmail.com>
2022-02-12 12:41:58 +0100

    optimize home page hero section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,24 +7,26 @@ function Home() {
   return (
     <main className="nav-offset text-center home-main">
       <section className="section-wrap">
-        <div className="hero flex">
-          <div className="hero-text text-white">
-            <h1 className="hero-heading text-h1">
-              Award-winning custom designs and digital branding solutions
-            </h1>
+        <div className="hero-wrapper">
+          <div className="hero flex">
+            <div className="hero-text text-white">
+              <h1 className="hero-heading text-h1">
+                Award-winning custom designs and digital branding solutions
+              </h1>
 
-            <p data-testid="home-subheaidng" className="hero-sub-heading">
-              With over 10 years in the industry, we are experienced in creating
-              fully responsive websites, app design, and engaging brand
-              experiences. Find out more about our services.
-            </p>
+              <p data-testid="home-subheaidng" className="hero-sub-heading">
+                With over 10 years in the industry, we are experienced in
+                creating fully responsive websites, app design, and engaging
+                brand experiences. Find out more about our services.
+              </p>
 
-            <div className="home-btn">
-              <Button path="about" btnText="learn more" btnTheme="btn-dark" />
+              <div className="home-btn">
+                <Button path="about" btnText="learn more" btnTheme="btn-dark" />
+              </div>
             </div>
-          </div>
 
-          <div className="hero-img"></div>
+            <div className="hero-img"></div>
+          </div>
         </div>
       </section>
 
